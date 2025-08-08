6ae0d2583573c0b81f842641d7e54b97519374d7
Enggita Septiana Nadeak <enjistudy@gmail.com>
2025-08-08 09:09:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const buttons = document.querySelectorAll(".button");
-const wrapperHolder = document.querySelector(".wrapper-holder");
-
-buttons.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    // Matikan animasi auto
-    wrapperHolder.style.animation = "none";
-
-    // Geser ke posisi sesuai tombol
-    wrapperHolder.style.transform = `translateX(-${index * 100}%)`;
-
-    // Bikin tombol aktif keliatan
-    buttons.forEach(btn => btn.classList.remove("active"));
-    button.classList.add("active");
-  });
-});
+function sayHello() {
+  alert("tengkiess sudaa baca sampee akhirr loafyuuuuuu");
+}
